2528c9a2e39f83eaf7c225edb74064ec0daf19e7
Adityaa11-code <winstrek212@gmail.com>
2025-12-15 17:36:07 +0700

    Commit No.1 - Adding a RecyclerView and Populating it

---
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.lab_week_06
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab_week_06.model.CatModel
+
+class CatAdapter(
+    private val layoutInflater: LayoutInflater,
+    private val imageLoader: ImageLoader
+) : RecyclerView.Adapter<CatViewHolder>() {
+
+    private val cats = mutableListOf<CatModel>()
+
+    fun setData(newCats: List<CatModel>) {
+        cats.clear()
+        cats.addAll(newCats)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
+        val view = layoutInflater.inflate(
+            R.layout.item_list,   // ← INI SEKARANG AMAN
+            parent,
+            false
+        )
+        return CatViewHolder(view, imageLoader)
+    }
+
+    override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
+        holder.bindData(cats[position])
+    }
+
+    override fun getItemCount(): Int = cats.size
+}
